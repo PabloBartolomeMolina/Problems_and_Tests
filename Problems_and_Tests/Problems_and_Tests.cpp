a2481523f94f83b8a5badae48bbb992158878aff
Pablo Bartolomé <pablobart.me@gmail.com>
2021-04-15 23:47:04 +0200

    Small modification to print some user's input. Useful to see how to push in Visual Studio 2019.

--- Problems_and_Tests/Problems_and_Tests.cpp
+++ Problems_and_Tests/Problems_and_Tests.cpp
@@ -3,10 +3,23 @@
 
 #include "Problems_and_Tests.h"
 
+#include <string>
+using std::string;
+using std::getline;
+
 using namespace std;
+
+string getInput()
+{
+	string inputText = "empty";
+	cout << "Type some words: ";	// Ask for any type of inout
+	getline(cin, inputText);		// Get user input from the keyboard
+	return inputText;
+}
+
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	cout << getInput() << endl;
 	return 0;
 }
