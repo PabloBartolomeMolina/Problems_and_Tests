077525d9878eec45977e72ffb67479eaf0381ad8
Pablo Bartolomé <pablobart.me@gmail.com>
2021-04-15 23:34:12 +0200

    Ajoutez des fichiers projet.

---
+++ Problems_and_Tests/Problems_and_Tests.cpp
@@ -0,0 +1,12 @@
+﻿// Problems_and_Tests.cpp : définit le point d'entrée de l'application.
+//
+
+#include "Problems_and_Tests.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
