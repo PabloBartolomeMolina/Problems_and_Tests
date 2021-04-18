b77666fea1f56d608ec2b8c6ac57b30a3fbdf1eb
Pablo Bartolomé <pablobart.me@gmail.com>
2021-04-18 15:52:21 +0200

    Psuh -- changes to sum function and add the a function to multiply

--- Problems_and_Tests/Problems_and_Tests.cpp
+++ Problems_and_Tests/Problems_and_Tests.cpp
@@ -6,11 +6,12 @@
 #include <string>
 #include <regex>
 
+
+using namespace std; 
+
 using std::string;
 using std::getline;
 using std::regex;
-
-using namespace std;
 
 string getInput(string demand)
 {
@@ -18,6 +19,24 @@ string getInput(string demand)
 	cout << demand;					// Ask for any type of input
 	getline(cin, inputText);		// Get user input from the keyboard
 	return inputText;
+}
+
+bool checkInput(string input)
+{
+	int temp = 0;
+	bool result = false;
+	if (regex_match(input, std::regex("[0-9]+")))
+	{
+		// It is a number, proceed with the sum.
+		result = true;
+	}
+	else
+	{
+		// It is not a number, error message displayed.
+		cout << "It is not a valid number and will not be considered." << endl;
+		result = false;
+	}
+	return result;
 }
 
 void sum()
@@ -29,7 +48,10 @@ void sum()
 	for (int i = 0; i < 2; i++)
 	{
 		tmp = getInput("Enter a number to sum: ");
-		if (regex_match(tmp, std::regex("[0-9]+")))
+
+		bool verify = checkInput(tmp);
+
+		if (verify)
 		{
 			// It is a number, proceed with the sum.
 			temp = stoi(tmp);
@@ -44,10 +66,46 @@ void sum()
 	cout << "Sum: " << sum << endl;
 }
 
+void multiply()
+{
+	int mult = 0;
+	int temp = 0;
+	string tmp = "0";
+	bool first = true;
+
+	for (int i = 0; i < 2; i++)
+	{
+		tmp = getInput("Enter a number to multiply: ");
+
+		bool verify = checkInput(tmp);
+
+		if (verify)
+		{
+			// It is a number, proceed with the sum.
+			temp = stoi(tmp);
+			if(first)
+			{
+				mult = temp;
+				first = !first;
+			}
+			else
+			{
+				mult = mult * temp;
+			}
+		}
+		else
+		{
+			// It is not a number, error message displayed.
+			cout << "It is not a valid number and will not be sum." << endl;
+		}
+	}
+	cout << "Result: " << mult << endl;
+}
+
 
 int main()
 {
 	sum();
-
+	multiply();
 	return 0;
 }
