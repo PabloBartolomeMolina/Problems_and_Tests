19c4e9fc77aa85bbb88eda98264dd0a21bffa3b0
Pablo Bartolomé <pablobart.me@gmail.com>
2021-04-18 16:03:29 +0200

    Push -- divide function and minor change to multiply function

--- Problems_and_Tests/Problems_and_Tests.cpp
+++ Problems_and_Tests/Problems_and_Tests.cpp
@@ -96,10 +96,51 @@ void multiply()
 		else
 		{
 			// It is not a number, error message displayed.
-			cout << "It is not a valid number and will not be sum." << endl;
+			cout << "It is not a valid number and will not be tqken into account." << endl;
 		}
 	}
 	cout << "Result: " << mult << endl;
+}
+
+
+void divide()
+{
+	float div = 0;
+	int temp = 0;
+	string tmp = "0";
+	bool first = true;
+
+	for (int i = 0; i < 2; i++)
+	{
+		tmp = getInput("Enter a number to divide: ");
+
+		bool verify = checkInput(tmp);
+
+		if (verify)
+		{
+			// It is a number, proceed with the sum.
+			temp = stoi(tmp);
+			if (first)
+			{
+				div = (float) temp;
+				first = !first;
+			}
+			else if (temp != 0)
+			{
+				div = (float) (div / temp);
+			}
+			else
+			{
+				cout << "It is not possible to divide by 0!!" << endl;
+			}
+		}
+		else
+		{
+			// It is not a number, error message displayed.
+			cout << "It is not a valid number and will not be tqken into account." << endl;
+		}
+	}
+	cout << "Result: " << div << endl;
 }
 
 
@@ -107,5 +148,6 @@ int main()
 {
 	sum();
 	multiply();
+	divide();
 	return 0;
 }
